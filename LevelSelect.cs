28eb0eb01c406e042e30c512d0d210f7f973b21a
lassiperala <44393346+lperala@users.noreply.github.com>
2021-01-06 20:42:29 +0200

    Add files via upload

---
+++ LevelSelect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    // Load a certain scene based on string input
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    // Exit the game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
